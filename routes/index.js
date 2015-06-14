ea6232af1307ab392444913632b3d191fb090d91
Teddi Maull <teddi@picturetheday.com>
2015-06-13 20:49:29 -0600

    Create a splash page with relevant information

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Image Finder' });
+  res.render('index', { title: 'find Me' });
 });
 
 module.exports = router;
