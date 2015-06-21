c1e62bd362c5bd7f86a840d9f8f010cb42566a3e
Teddi Maull <teddi@picturetheday.com>
2015-06-20 20:50:28 -0600

    Organize the code, write new methods for accessing collection data

--- routes/find_photos.js
+++ routes/find_photos.js
@@ -1,5 +1,4 @@
-function findPhotos() {
-// define how to keep track of photos
+function findPhotos(urls) {
   var request = require('request');
 
   var Photo = function(url) {
@@ -12,6 +11,7 @@ function findPhotos() {
     this.targetPhotoId = '';
     // example: {url: [id1,id2,id3], url2: [id4], url3: [id5,id6,id7]}
     this.allUrlsAndFaceIds = [];
+    this.faceIdData = {};
 
     var that = this;
 
@@ -20,7 +20,10 @@ function findPhotos() {
         that.targetPhotoId = photo.faceId[0] || '';
       },
       addPhoto: function(photo) {
-        that.allUrlsAndFaceIds[photo.mainUrl] = photo.faceIds;
+        that.allUrlsAndFaceIds[photo.mainUrl] = [];
+      },
+      addPhotoFaceId: function(photo) {
+        that.allUrlsAndFaceIds[photo.mainUrl] = photo.faceIds[0];
       },
       getPhotoUrls: function() {
         return Object.keys(that.allUrls);
@@ -43,50 +46,15 @@ function findPhotos() {
         }
       },
       getFaceIdData: function() {
-        var faceIdData["faceId"] = that.targetPhotoId;
-        faceIdData["faceIds"] = this.getAllFaceIds;
-        return faceIdData;
+        that.faceIdData["faceId"] = that.targetPhotoId;
+        that.faceIdData["faceIds"] = this.getAllFaceIds();
+        return that.faceIdData;
       }
     }
   };
 
-// need to get the urls from cloudinary
-//temp, eventually all of this needs to be received from the client side in a request
-
-    var photo1 = new Photo("http://res.cloudinary.com/isityou/image/upload/v1434088990/get_faces/codersgonnacode.png");
-    var photo2 = new Photo("http://res.cloudinary.com/isityou/image/upload/v1433997424/preset_folder/yv7u6cn6x8p9ahhqngti.jpg");
-
-    var result = [photo1, photo2];
-    var userPhotoCollection = new CollectionOfPhotos();
-    result.forEach(function(photoFromCloudinary) {
-          userPhotoCollection.addPhoto(photoFromCloudinary);
-        });
-
-    var params = {
-      'subscription-key': process.env.OXFORD_API_SECOND_KEY
-    };
-
-    var comparisonPhotos = new CollectionOfPhotos();
-    comparisonPhotos.addPhoto(photo1);
-    console.log("photo1 Object:" + photo1);
-
-    var dataUrl = {'url': 'http://res.cloudinary.com/isityou/image/upload/c_scale,w_597/v1433993584/jqtep3a6uqc80bhterye.jpg'};
-    // {'url': photo1.mainUrl };
-
-  // get the target photo from the user. This functionality has not been added to the view yet.
-    // console.log("faceDetection results: " + faceDetection(dataUrl));
-    faceDetection(dataUrl).then(findSimilars(faceIds));
-    // comparisonPhotos.addTargetPhoto(photo1);
-    // console.log("photo1 Ids:" + photo1.faceIds);
-
-    // get the faceIds for each photo
-    var photosUrls = comparisonPhotos.getPhotoUrls();
-    // var photoIds = photoUrls.map(function(photoUrl) {
-      // faceDetection(photoUrl);
-    // })
-
-
   var faceDetection = function(dataUrls) {
+    //if dataUrls.length is 0 then return the getFaceIdData and call findSimilars?
     var options={
                 'APIkey':process.env.OXFORD_API_SECOND_KEY
               };
@@ -99,17 +67,16 @@ function findPhotos() {
              url: 'https://api.projectoxford.ai/face/v0/detections',
              headers: {'Access-Control-Allow-Origin': '*', 'Ocp-Apim-Subscription-Key': process.env.OXFORD_API_SECOND_KEY},
              method:"POST",
-             body: JSON.stringify(dataUrl)
+             body: JSON.stringify(dataUrls[0])
            },function(error,response,body){
                 console.log("body:" + body);
                  //get the faceId of the photo and push it onto the id list
-                var parsedBody = JSON.parse(body);
-                 photo1.faceIds.push(parsedBody[0]["faceId"]);
-                 photo1.faceCoordinates.push(parsedBody[0]["faceRectangle"]);
-                 console.log("photo1 Ids:" + photo1.faceIds);
-                 console.log(photo1);
-                 console.log("this:" + this);
 
+                var parsedBody = JSON.parse(body);
+                //I no longer have named photos...so I need to figure out another way of doing this
+                photo1.faceIds.push(parsedBody[0]["faceId"]);
+                photo1.faceCoordinates.push(parsedBody[0]["faceRectangle"]);
+               // call FaceDetection with an array that has the first element cut off?
              });
   }
 
@@ -136,7 +103,27 @@ function findPhotos() {
              });
 
   }
-    return 'userPhotoCollection.allUrls';
+
+  var userPhotoCollection = new CollectionOfPhotos();
+
+  ////////////////////////////////////////////////////////////
+    // need to get the urls from cloudinary
+    //below is temporary so I can test other components
+
+    urls = ["http://res.cloudinary.com/isityou/image/upload/v1434088990/get_faces/codersgonnacode.png",
+    "http://res.cloudinary.com/isityou/image/upload/v1433997424/preset_folder/yv7u6cn6x8p9ahhqngti.jpg"
+    ];
+  ////////////////////////////////////////////////////////////
+
+
+////////////this doesn't work either, still need to figure out promises
+  var matchedFaceIds = urls.forEach(function(url){faceDetection(url);}).then(findSimilars(userPhotoCollection.getFaceIdData()));
+
+  var matchedUrls = matchedFaceIds.map(function(faceId) {return userPhotoCollection.getPhotoUrlFromFaceId(faceId)});
+
+//////////////this will return an object with the matchedUrls (not a string)
+  return 'matchedUrls';
+
 }
 
 module.exports = findPhotos;
