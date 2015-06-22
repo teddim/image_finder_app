52a70116bb5ae4e4566e30a65736aedb7c36a2d2
Teddi Maull <teddi@picturetheday.com>
2015-06-22 17:09:13 -0600

    Try using Q promises to solve async problems

--- routes/find_photos.js
+++ routes/find_photos.js
@@ -1,5 +1,6 @@
 function findPhotos(urls) {
   var request = require('request');
+  var Q = require('q');
 
   var Photo = function(url) {
     this.faceIds = [];
@@ -8,13 +9,18 @@ function findPhotos(urls) {
   };
 
   var CollectionOfPhotos = function() {
-    this.targetPhotoId = '';
+    this.targetPhotoId = '94fde889-b591-4216-b556-41eb3f7def22';
     // example: {url: [id1,id2,id3], url2: [id4], url3: [id5,id6,id7]}
-    this.allUrlsAndFaceIds = [];
+    this.allUrlsAndFaceIds = {"http://res.cloudinary.com/isityou/image/upload/v1434854280/get_faces/heather.jpg": ['9212a496-a4a2-4697-ba35-e866bb43bef5']
+    ,"http://res.cloudinary.com/isityou/image/upload/v1434891981/sue_v3l6yq.jpg": ['260d9d11-504a-4eff-8b0f-c51d5f10debc']
+    ,"http://res.cloudinary.com/isityou/image/upload/v1433997424/preset_folder/yv7u6cn6x8p9ahhqngti.jpg" : ['985425cc-1ad4-43ee-b80d-2d6ee5797e25']
+    ,"http://res.cloudinary.com/isityou/image/upload/v1434854174/get_faces/maria.jpg" : ['3a8d2c6a-deeb-4ddf-a17a-6c2716b911d8']
+    ,"http://res.cloudinary.com/isityou/image/upload/v1434892101/maria2_i05l5c.jpg" : ['94fde889-b591-4216-b556-41eb3f7def22']
+    };
     this.faceIdData = {};
+    this.matchedUrls = [];
 
     var that = this;
-
     return {
       addTargetPhoto: function(photo) {
         that.targetPhotoId = photo.faceId[0] || '';
@@ -26,10 +32,10 @@ function findPhotos(urls) {
         that.allUrlsAndFaceIds[photo.mainUrl] = photo.faceIds[0];
       },
       getPhotoUrls: function() {
-        return Object.keys(that.allUrls);
+        return Object.keys(that.allUrlsAndFaceIds);
       },
       getAllFaceIds: function() {
-        var faceIds = [];
+        var faceIds = []
         for (key in that.allUrlsAndFaceIds) {
           faceIds = faceIds.concat(that.allUrlsAndFaceIds[key]);
         }
@@ -49,14 +55,20 @@ function findPhotos(urls) {
         that.faceIdData["faceId"] = that.targetPhotoId;
         that.faceIdData["faceIds"] = this.getAllFaceIds();
         return that.faceIdData;
+      },
+      addMatchedUrls: function(matchedUrls) {
+        that.matchedUrls = matchedUrls;
+      },
+      getMatchedUrls: function() {
+        console.log("get", that.matchedUrls);
+        return that.matchedUrls;
       }
     }
   };
 
   var faceDetection = function(dataUrl) {
     var dataUrl = {'url': dataUrl};
-    console.log("dataUrl:" + dataUrl);
-    //if dataUrls.length is 0 then return the getFaceIdData and call findSimilars?
+    console.log("dataUrl:" , dataUrl);
     var options={
                 'APIkey':process.env.OXFORD_API_SECOND_KEY
               };
@@ -67,10 +79,12 @@ function findPhotos(urls) {
              method:"POST",
              body: JSON.stringify(dataUrl)
            },function(error,response,body){
-                console.log("body:" + body);
+                // console.log("body:" + body);
                  //get the faceId of the photo and push it onto the id list
 
                 var parsedBody = JSON.parse(body);
+                console.log("faceDetection: ");
+
                 console.log(parsedBody);
                 //I no longer have named photos...so I need to figure out another way of doing this
                 // photo1.faceIds.push(parsedBody[0]["faceId"]);
@@ -80,27 +94,27 @@ function findPhotos(urls) {
   }
 
   var findSimilars = function (faceIdData) {
-
+    // console.log(faceIdData);
     var options={
                 'APIkey':process.env.OXFORD_API_SECOND_KEY
               };
-    var temp = [];
-    var func = function(data){
-      temp.push(data);
-      console.log(temp);
-    }
+
     request({
              url: 'https://api.projectoxford.ai/face/v0/findsimilars',
              headers: {'Access-Control-Allow-Origin': '*', 'Ocp-Apim-Subscription-Key': process.env.OXFORD_API_SECOND_KEY},
              method:"POST",
              body: JSON.stringify(faceIdData)
            },function(error,response,body){
-                console.log("body:" + body);
                  //get the faceId of the photo and push it onto the id list
                 var parsedBody = JSON.parse(body);
-                 console.log("body: "+ parsedBody);
+                 console.log("findSimilars: ");
+                 console.log(parsedBody);
+                 var matchedUrls = parsedBody.map(function(element) {
+                   return userPhotoCollection.getPhotoUrlFromFaceId(element.faceId);
+                 });
+                 userPhotoCollection.addMatchedUrls(matchedUrls);
+                 console.log('urls',userPhotoCollection.getMatchedUrls());
              });
-
   }
 
   var userPhotoCollection = new CollectionOfPhotos();
@@ -109,19 +123,32 @@ function findPhotos(urls) {
     // need to get the urls from cloudinary
     //below is temporary so I can test other components
 
-    urls = ["http://res.cloudinary.com/isityou/image/upload/v1434088990/get_faces/codersgonnacode.png",
-    "http://res.cloudinary.com/isityou/image/upload/v1433997424/preset_folder/yv7u6cn6x8p9ahhqngti.jpg"
+    urls = ["http://res.cloudinary.com/isityou/image/upload/v1434854280/get_faces/heather.jpg"
+    ,"http://res.cloudinary.com/isityou/image/upload/v1434891981/sue_v3l6yq.jpg"
+    ,"http://res.cloudinary.com/isityou/image/upload/v1433997424/preset_folder/yv7u6cn6x8p9ahhqngti.jpg"
+    ,"http://res.cloudinary.com/isityou/image/upload/v1434854174/get_faces/maria.jpg"
+    ,"http://res.cloudinary.com/isityou/image/upload/v1434892101/maria2_i05l5c.jpg"
     ];
   ////////////////////////////////////////////////////////////
 
 
-////////////this doesn't work either, still need to figure out promises
-  var matchedFaceIds = urls.forEach(function(url){faceDetection(url);}).then(findSimilars(userPhotoCollection.getFaceIdData()));
+var promised = Q("initialVal");
+//var promised = deferred.promise;
 
-  var matchedUrls = matchedFaceIds.map(function(faceId) {return userPhotoCollection.getPhotoUrlFromFaceId(faceId)});
+// //look at the reduce syntax
+urls.forEach(function (url) {
+    promised = promised
+                .then( function(val) { faceDetection(url); console.log("%j",val); return "success"; } , function(){return "error";});
+  });
 
-//////////////this will return an object with the matchedUrls (not a string)
-  return 'matchedUrls';
+promised = promised
+   .then( function(val) { findSimilars(userPhotoCollection.getFaceIdData()); console.log("lst promise %j",val); return true; },function(){return "error in findSimilars";} )
+
+//deferred.resolve(userPhotoCollection.getMatchedUrls());
+
+console.log("here we are : %j",promised);
+
+return userPhotoCollection.getMatchedUrls();
 
 }
 
--- routes/photo_upload.js
+++ routes/photo_upload.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var findPhotos = require('./find_photos');
+var Q = require('q');
+
 var router = express.Router();
 
 
@@ -14,7 +16,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-  res.json({photos: findPhotos()});
+  // res.json({photos: [ 'http://res.cloudinary.com/isityou/image/upload/v1434892101/maria2_i05l5c.jpg',
+  // 'http://res.cloudinary.com/isityou/image/upload/v1434854174/get_faces/maria.jpg' ]});
+  Q.fcall(findPhotos).then(function(urls) {res.json({photos: urls});}).done();
+
+  // res.json({photos: "findPhotos()"});
 });
 
 
