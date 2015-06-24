9ec23f81e2261b878528e14c45155186558e64a7
Teddi Maull <teddi@picturetheday.com>
2015-06-23 22:36:39 -0600

    Change app name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'find Me' });
+  res.render('index', { title: 'My Image Finder' });
 });
 
 module.exports = router;
