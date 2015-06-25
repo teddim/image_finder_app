f0fd68661f78974b2ed4308429a0d59c0f38a91e
Teddi Maull <teddi@picturetheday.com>
2015-06-25 01:40:57 -0600

    add styling

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,46 @@
 $(document).ready(function() {
       // upload images to cloudinary
 
+    // $('#target_photo').cloudinary_upload_widget(
+    //   { cloud_name: 'isityou', upload_preset: 'get_faces',
+    //   'folder': 'get_faces', theme: 'minimal', sources: ['local'], thumbnail_transformation: { width: 200, height: 200, crop: 'limit' }, button_caption: 'Upload a Target Image', multiple: false },
+    //   function(error, result) {
+    //     console.log("target: ", result[0].url);
+    //     localStorage.setItem("target",result[0].url);
+    //   }
+    // );
+
     $('#target_photo').cloudinary_upload_widget(
-      { cloud_name: 'isityou', upload_preset: 'get_faces',
-      'folder': 'get_faces', theme: 'minimal', thumbnail_transformation: { width: 200, height: 200, crop: 'limit' }, button_caption: 'Upload a Target Image', multiple: false },
+      { cloud_name: 'isityou',
+      upload_preset: 'get_faces',
+      'folder': 'get_faces',
+      theme: 'minimal', sources: ['local'],
+      thumbnail_transformation: { width: 200, height: 200, crop: 'limit' },
+      button_caption: 'Upload a Target Image',
+      multiple: false,
+      button_class: "btn btn-default target_button",
+      max_image_height: 800,
+      max_image_width: 800,
+      // thumbnails: ".target_photo"
+    },
       function(error, result) {
         console.log("target: ", result[0].url);
         localStorage.setItem("target",result[0].url);
-      }
-    );
+    });
 
-    $('#upload_widget_opener_collection').cloudinary_upload_widget(
+    $('#collection_photos').cloudinary_upload_widget(
       { cloud_name: 'isityou',
       upload_preset: 'get_faces',
       'folder': 'get_faces',
       theme: 'minimal',
+      sources: ['local'],
       thumbnail_transformation: { width: 200, height: 200, crop: 'limit' },
       button_caption: 'Upload a Collection of Images',
-      max_files: 20
+      max_files: 20,
+      button_class: "btn btn-default collection_button",
+      max_image_height: 800,
+      max_image_width: 800,
+      // thumbnails:".collection_photos"
       },
       function(error, results) {
         console.log("Cloudinary result:", results[0]);
@@ -33,13 +56,13 @@ $(document).ready(function() {
 
         console.log("resultObj:", resultObj);
         // console.log("json:", json);
-// debugger;
+    // debugger;
 
         $.post( "/photo_upload", { targeturl: localStorage.getItem("target"), urls: cloudinaryUrls })
           .done(function( photoUrls ) {
             console.log(photoUrls);
             $.each( photoUrls, function( key, url ) {
-              var imgSrc = ("<img src = '" + url + "'/>");
+              var imgSrc = ("<img src = '" + url + "' class = 'cloudimage'/>");
               $("#matched_photos").append(imgSrc);
             });
           });
