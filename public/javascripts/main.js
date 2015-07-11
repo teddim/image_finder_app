94caff6dcfca2972584d188676413959ae10f541
Teddi Maull <teddi@picturetheday.com>
2015-07-11 10:00:14 -0600

    remove cruft and fix bug that allows multiple target images to be added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-      // upload images to cloudinary
-
-    // $('#target_photo').cloudinary_upload_widget(
-    //   { cloud_name: 'isityou', upload_preset: 'get_faces',
-    //   'folder': 'get_faces', theme: 'minimal', sources: ['local'], thumbnail_transformation: { width: 200, height: 200, crop: 'limit' }, button_caption: 'Upload a Target Image', multiple: false },
-    //   function(error, result) {
-    //     console.log("target: ", result[0].url);
-    //     localStorage.setItem("target",result[0].url);
-    //   }
-    // );
 
     $('#target_photo').cloudinary_upload_widget(
       { cloud_name: 'isityou',
@@ -21,11 +11,12 @@ $(document).ready(function() {
       button_class: "btn btn-default target_button",
       max_image_height: 800,
       max_image_width: 800,
-      // thumbnails: ".target_photo"
+      // thumbnails: "#target_photo"
     },
       function(error, result) {
-        console.log("target: ", result[0].url);
         localStorage.setItem("target",result[0].url);
+        document.getElementsByClassName('target_button')[0].style.display = 'none';
+        document.getElementsByClassName('target_photo_text')[0].innerHTML = 'I am looking for:';
     });
 
     $('#collection_photos').cloudinary_upload_widget(
@@ -43,24 +34,18 @@ $(document).ready(function() {
       // thumbnails:".collection_photos"
       },
       function(error, results) {
-        console.log("Cloudinary result:", results[0]);
+        document.getElementsByClassName('collection_button')[0].style.display = 'none';
+        document.getElementsByClassName('collection_photo_text')[0].innerHTML = 'in this collection of photos:';
         var cloudinaryUrls = results.map(function(result){
           return result.url;
           })
-          console.log("cloudinaryUrls:", cloudinaryUrls);
 
         var resultObj = {};
         resultObj["targeturl"] = localStorage.getItem("target");
         resultObj["urls"] = cloudinaryUrls;
-        // var json = JSON.stringify(resultObj);
-
-        console.log("resultObj:", resultObj);
-        // console.log("json:", json);
-    // debugger;
 
         $.post( "/photo_upload", { targeturl: localStorage.getItem("target"), urls: cloudinaryUrls })
           .done(function( photoUrls ) {
-            console.log(photoUrls);
             $.each( photoUrls, function( key, url ) {
               var imgSrc = ("<img src = '" + url + "' class = 'cloudimage'/>");
               $("#matched_photos").append(imgSrc);
